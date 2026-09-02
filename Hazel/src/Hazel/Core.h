426466028800c026cc123119662600d95ac69415
OverrideTuring <1506760246@qq.com>
2026-09-03 02:17:47 +0800

    引入spdlog作为日志

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #ifdef HZ_PLATFORM_WINDOWS
 	#ifdef HZ_BUILD_DLL
@@ -9,3 +9,17 @@
 #else
 	#error Hazel only supports Windows!
 #endif
+
+// Core log macros
+#define HZ_CORE_TRACE(...)		::Hazel::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define HZ_CORE_INFO(...)		::Hazel::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define HZ_CORE_WARN(...)		::Hazel::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define HZ_CORE_ERROR(...)		::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define HZ_CORE_CRITICAL(...)	::Hazel::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+// Client log macros
+#define HZ_TRACE(...)			::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HZ_INFO(...)			::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
+#define HZ_WARN(...)			::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define HZ_ERROR(...)			::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
+#define HZ_CRITICAL(...)		::Hazel::Log::GetClientLogger()->critical(__VA_ARGS__)
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,10 +1,16 @@
-#pragma once
+﻿#pragma once
 
 #ifdef HZ_PLATFORM_WINDOWS
 
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
+	Hazel::Log::Init();
+	HZ_CORE_INFO("Hazel Engine starts.");
+	HZ_CORE_WARN("Hazel Engine starts.");
+	HZ_CORE_ERROR("Hazel Engine starts.");
+	HZ_CORE_CRITICAL("Hazel Engine starts.");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
