26bca9bccb672dd609c177af4df1aa770f535c7d
OverrideTuring <1506760246@qq.com>
2026-09-07 02:08:43 +0800

    增加 premake5.lua 文件

--- Hazel/src/Hazel/Log.cpp
+++ Hazel/src/Hazel/Log.cpp
@@ -1,4 +1,4 @@
-#include "Log.h"
+﻿#include "Log.h"
 
 namespace Hazel {
 
@@ -12,7 +12,7 @@ namespace Hazel {
 		s_CoreLogger->set_level(spdlog::level::level_enum::trace);
 
 		s_ClientLogger = spdlog::stdout_color_mt("APP");
-		s_CoreLogger->set_level(spdlog::level::level_enum::trace);
+		s_ClientLogger->set_level(spdlog::level::level_enum::trace);
 	}
 
 }
