ff8d33e77382914743589783ac6ca20be0e81cdf
OverrideTuring <1506760246@qq.com>
2026-09-20 05:12:08 +0800

    添加预编译头

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,4 +1,5 @@
-﻿#include "Application.h"
+﻿#include "hzpch.h"
+#include "Application.h"
 
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
--- Hazel/src/Hazel/Log.cpp
+++ Hazel/src/Hazel/Log.cpp
@@ -1,4 +1,5 @@
-﻿#include "Log.h"
+﻿#include "hzpch.h"
+#include "Log.h"
 
 namespace Hazel {
 
